64482606bcaddfc863c2ef1cafd1e64ddf071244
Hdavid <hajnal.david@erise.hu>
2016-07-10 18:23:17 +0200

    test server communication

---
+++ src/test/java/io/hajnal/david/sentinel_integration_test/utils/TestNotifyServer.java
@@ -0,0 +1,76 @@
+package io.hajnal.david.sentinel_integration_test.utils;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import io.hajnal.david.sentinel.communication.Message;
+import io.hajnal.david.sentinel.communication.SocketConnection;
+import io.hajnal.david.sentinel.communication.Socket.AbstractSocketFactory;
+import io.hajnal.david.sentinel.config.CommunicationConfig;
+import io.hajnal.david.sentinel_integration_test.utils.helpers.TcpServer;
+import junit.framework.Assert;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = CommunicationConfig.class)
+public class TestNotifyServer {
+
+	private static final String TEST_MESSAGE = "echo";
+	private static final String TEST_MESSAGE2 = "echoöüóűúőéá";
+	private static final String TEST_ID = "test.echo";
+
+	@Autowired
+	private ApplicationContext context;
+
+	private SocketConnection underTest;
+
+	private static TcpServer server;
+
+	@BeforeClass
+	public static void setUpOnce() throws IOException {
+		server = new TcpServer();
+		new Thread(server).start();
+	}
+
+	@AfterClass
+	public static void tearDownOnce() throws Exception {
+		server.close();
+	}
+
+	@Before
+	public void setUp() {
+		underTest = new SocketConnection(TcpServer.HOST, TcpServer.PORT);
+		AbstractSocketFactory socketFactory = context.getBean(AbstractSocketFactory.class);
+		underTest.setSocketFactory(socketFactory);
+	}
+
+	@Test
+	public void serverShouldReceiveTheMessage() throws InterruptedException {
+		// Given
+		Message<String> message = new Message<>(TEST_ID, TEST_MESSAGE);
+		// When
+		underTest.sendMessage(message);
+		// Then
+		Thread.sleep(100);
+		Assert.assertTrue(server.getMessageList().contains(message));
+	}
+	
+	@Test
+	public void serverShouldReceiveAnotherTheMessage() throws InterruptedException {
+		// Given
+		Message<String> message = new Message<>(TEST_ID, TEST_MESSAGE2);
+		// When
+		underTest.sendMessage(message);
+		// Then
+		Thread.sleep(100);
+		Assert.assertTrue(server.getMessageList().contains(message));
+	}
+}
---
+++ src/test/java/io/hajnal/david/sentinel_integration_test/utils/helpers/TcpServer.java
@@ -0,0 +1,61 @@
+package io.hajnal.david.sentinel_integration_test.utils.helpers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.hajnal.david.sentinel.communication.Message;
+
+public class TcpServer implements Runnable, AutoCloseable {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TcpServer.class);
+
+	public static final int PORT = 9999;
+	public static final String HOST = "127.0.0.1";
+
+	private volatile boolean isRun = true;
+	private ServerSocket serverSocket;
+	private List<Message<String>> messageList;
+
+	public TcpServer() throws IOException {
+		serverSocket = new ServerSocket(PORT);
+		messageList = new ArrayList<>();
+	}
+
+	public List<Message<String>> getMessageList() {
+		return messageList;
+	}
+
+	@Override
+	public void run() {
+		LOGGER.info("TcpServer start");
+		while (isRun) {
+			try {
+				Socket connectionSocket = serverSocket.accept();
+				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+				String dataFromClient = inFromClient.readLine();
+				LOGGER.debug("Got data: " + dataFromClient);
+				Message<String> result = Message.of(dataFromClient);
+				messageList.add(result);
+			} catch (SocketException e) {
+				LOGGER.info("TcpServer socket closed");
+			} catch (IOException e) {
+				LOGGER.error(e.getMessage(), e);
+			}
+		}
+	}
+
+	@Override
+	public void close() throws Exception {
+		isRun = false;
+		serverSocket.close();
+	}
+}
